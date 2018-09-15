4efb2ff40c226c40746d1f0909d113583fbf808e
Kenneth Tran <quoc.tran@okstate.edu>
2018-09-15 15:06:08 -0500

    first commit

---
+++ src/components/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-form>
+            <v-text-field type="text" @blur="reset" placeholder="Enter anything to look for recipe">
+
+            </v-text-field>
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage'
+}
+</script>
